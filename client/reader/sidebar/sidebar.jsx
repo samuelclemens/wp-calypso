766ead5679816873c145e70593ced9a28b7702ea
bluefuton <chris@bluefuton.com>
2015-12-03 08:35:16 +1100

    Clear sidebar input in SidebarMenu, not Sidebar (elements have moved)

--- client/reader/sidebar/sidebar.jsx
+++ client/reader/sidebar/sidebar.jsx
@@ -30,8 +30,6 @@ const layoutFocus = require( 'lib/layout-focus' ),
 	stats = require( 'reader/stats' ),
 	Gridicon = require( 'components/gridicon' ),
 	discoverHelper = require( 'reader/discover/helper' ),
-	Button = require( 'components/button' ),
-	Count = require( 'components/count' ),
 	config = require( 'config' ),
 	ExpandableSidebarMenu = require( './menu' );
 
@@ -149,7 +147,6 @@ module.exports = React.createClass( {
 	highlightNewList: function( list ) {
 		list = ReaderListsStore.get( list.owner, list.slug );
 		window.location.href = url.resolve( 'https://wordpress.com', url.resolve( list.URL, 'edit' ) );
-		ReactDom.findDOMNode( this.refs.menuAddInput ).value = '';
 	},
 
 	highlightNewTag: function( tag ) {
@@ -157,7 +154,6 @@ module.exports = React.createClass( {
 			page( '/tag/' + tag.slug );
 			window.scrollTo( 0, 0 );
 		} );
-		ReactDom.findDOMNode( this.refs.menuAddInput ).value = '';
 	},
 
 	createList: function( list ) {
@@ -184,7 +180,7 @@ module.exports = React.createClass( {
 	renderLists: function() {
 		if ( ! this.state.lists ) {
 			return (
-				[ <li className="sidebar-menu__empty">{ this.translate( 'Collect sites together by adding a\xa0list.' ) }</li> ]
+				[ <li key="empty" className="sidebar-menu__empty">{ this.translate( 'Collect sites together by adding a\xa0list.' ) }</li> ]
 			);
 		}
 
@@ -229,7 +225,7 @@ module.exports = React.createClass( {
 	renderTags: function() {
 		if ( ! this.state.tags || this.state.tags.length === 0 ) {
 			return (
-				[ <li className="sidebar-menu__empty">{ this.translate( 'Finds relevant posts by adding a\xa0tag.' ) }</li> ]
+				[ <li key="empty" className="sidebar-menu__empty">{ this.translate( 'Finds relevant posts by adding a\xa0tag.' ) }</li> ]
 			);
 		}
 
@@ -285,16 +281,8 @@ module.exports = React.createClass( {
 	render: function() {
 		let followingEditLink = this.getFollowingEditLink();
 
-		var tagCount = 0,
-			listCount = 0;
-
-		if ( typeof this.state.tags !== 'undefined' && this.state.tags !== null && this.state.tags.length > 0 ) {
-			tagCount = this.state.tags.length;
-		}
-
-		if ( typeof this.state.lists !== 'undefined' && this.state.lists !== null && this.state.lists.length > 0 ) {
-			listCount = this.state.lists.length;
-		}
+		const tagCount = this.state.tags ? this.state.tags.length : 0,
+			listCount = this.state.lists ? this.state.lists.length : 0;
 
 		return (
 			<Sidebar onClick={ this.handleClick }>
