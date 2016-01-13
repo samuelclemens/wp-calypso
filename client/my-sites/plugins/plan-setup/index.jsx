5da930c7f1d50ffababa508fe127413e8624e323
Kelly Dwan <kelly.dwan@gmail.com>
2016-01-13 12:12:06 -0500

    Start the plugin provisioning framework

--- client/my-sites/plugins/plan-setup/index.jsx
+++ client/my-sites/plugins/plan-setup/index.jsx
@@ -6,12 +6,26 @@ import React from 'react'
 /**
  * Internal dependencies
  */
+import PluginInstallation from './installation';
 
 module.exports = React.createClass( {
 
 	displayName: 'PlanSetup',
 
+	runInstall: function() {
+		let steps = PluginInstallation.start( {
+			site: this.props.selectedSite,
+			plugins: [ 'vaultpress' ]
+		} );
+
+		steps.on( 'data', ( step ) => {
+			console.log( step );
+			// Update component state
+		} );
+	},
+
 	render() {
+		this.runInstall();
 		return (
 			<div>
 				<h1>Setting up your plan…</h1>
