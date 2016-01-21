421d3e080835ad4bc79398a8d4340ffc3b66b687
Ben Dwyer <ben@scruffian.com>
2016-01-21 17:48:57 +0000

    Catch Errors: Only use local storage when its supported

--- public/catch-js-errors.js
+++ public/catch-js-errors.js
@@ -1,4 +1,5 @@
-var stringifyErrorForUrlParams = function( error ) {
+// Props http://stackoverflow.com/a/20405830/379063
+function stringifyErrorForUrlParams( error ) {
 	var simpleObject = {};
 	Object.getOwnPropertyNames( error ).forEach( function( key ) {
 		simpleObject[ key ] = encodeURIComponent( error[ key ] );
@@ -6,8 +7,48 @@ var stringifyErrorForUrlParams = function( error ) {
 	return JSON.stringify( simpleObject );
 };
 
-function sendErrorsToAPI( message, scriptUrl, lineNumber, columnNumber, error ) {
-	var xhr = new XMLHttpRequest(), params;
+// Props http://stackoverflow.com/a/17604754/379063
+function isLocalStorageNameSupported() {
+	var testKey = 'test',
+		storage = window.localStorage;
+	try {
+		storage.setItem( testKey, '1' );
+		storage.removeItem( testKey );
+		return true;
+	} catch ( error ) {
+		return false;
+	}
+}
+
+/**
+ * Throttle function by delay ms
+ *
+ * @param {function} fn wrapped function
+ * @param {number} delay minimum debounce time in ms
+ * @returns {function} a callback
+ */
+function debounce( fn, delay ) {
+	var lastCall = Date.now();
+
+	return function() {
+		var now = Date.now();
+
+		if ( ( now - lastCall ) < delay ) {
+			return;
+		}
+
+		lastCall = now;
+		fn.apply( null, arguments );
+	}
+}
+
+var sendErrorsToApiDebounced = debounce( function( message, scriptUrl, lineNumber, columnNumber, error ) {
+	sendErrorsToApi( message, scriptUrl, lineNumber, columnNumber, error );
+}, 100 );
+
+function sendErrorsToApi( message, scriptUrl, lineNumber, columnNumber, error ) {
+	var xhr = new XMLHttpRequest(),
+		params;
 
 	error = error || new Error( message );
 
@@ -24,9 +65,9 @@ function sendErrorsToAPI( message, scriptUrl, lineNumber, columnNumber, error ) 
 	xhr.send( params );
 }
 
-if ( ( localStorage.getItem( 'log-errors' ) !== undefined && localStorage.getItem( 'log-errors' ) === 'true' ) || Math.random() <= 0.01 ) {
+if ( isLocalStorageNameSupported && ( localStorage.getItem( 'log-errors' ) !== undefined && localStorage.getItem( 'log-errors' ) === 'true' ) || Math.random() <= 0.01 ) {
 	localStorage.setItem( 'log-errors', true );
 
 	// set up handler to POST errors
-	window.onerror = sendErrorsToAPI;
+	window.onerror = sendErrorsToApiDebounced;
 }
