0f7d6c9db1482f4870b5e59f8b4b35597d8fb0c8
Samuel Clemens <yury@michurin.co.il>
2016-01-30 17:04:26 +0200

    Initial selector

--- client/state/comments/selectors.js
+++ client/state/comments/selectors.js
@@ -1,3 +1,11 @@
 /**
  * Created by yury on 1/29/16.
  */
+
+import {
+	commentTargetId
+} from './utils';
+
+export function getComments( state, siteId, postId ) {
+	return state.comments.items.get( commentTargetId( siteId, postId ) );
+}
