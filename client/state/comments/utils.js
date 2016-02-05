3c4af23b24e565cec3f92941ddc4b848d9372c8f
Samuel Clemens <yury@michurin.co.il>
2016-02-05 22:01:14 +0200

    Moved the formatting url function to utils

--- client/state/comments/utils.js
+++ client/state/comments/utils.js
@@ -2,10 +2,27 @@
  * Created by yury on 1/29/16.
  */
 
-export function createCommentTargetId(siteId, postId ) {
-	return `${siteId}-${postId}`;
+export function createCommentTargetId( siteId, postId ) {
+	return `${ siteId }-${ postId }`;
 }
 
 export function createRequestId( siteId, postId, query ) {
-	return `${siteId}-${postId}-${JSON.stringify(query)}`;
+	return `${siteId}-${postId}-${ JSON.stringify( query ) }`;
+}
+
+export function createUrlFromTemplate( urlTemplate, urlParams, queryParams ) {
+	let constructedUrl = urlTemplate;
+
+	Object.keys( urlParams ).forEach( ( key ) => {
+		const regex = new RegExp( '\\$' + key, 'g' );
+		constructedUrl = constructedUrl.replace( regex, urlParams[ key ] );
+	} );
+
+	if ( queryParams ) {
+		constructedUrl += '?' + Object.keys( queryParams )
+				.map( ( param ) => param + '=' + encodeURIComponent( queryParams[ param ] ) )
+				.join( '&' );
+	}
+
+	return constructedUrl;
 }
